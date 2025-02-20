cc8bbb328b002c63cd6d29f50d8835121f6f985d
Ragib Asif <182296466+ragibasif@users.noreply.github.com>
2025-02-20 15:45:15 -0500

    {{UPDATE}} Thu Feb 20 15:45:15 2025

--- crafting-interpreters-clox/chunk.c
+++ crafting-interpreters-clox/chunk.c
@@ -7,10 +7,13 @@ void initChunk(Chunk* chunk) {
     chunk->count = 0;
     chunk->capacity = 0;
     chunk->code = NULL;
+    chunk->lines = NULL;
+    initValueArray(&chunk->constants);
 }
 
 void freeChunk(Chunk* chunk) {
     FREE_ARRAY(uint8_t, chunk->code, chunk->capacity);
+    freeValueArray(&chunk->constants);
     initChunk(chunk);
 }
 
@@ -23,3 +26,10 @@ void writeChunk(Chunk* chunk, uint8_t byte) {
     chunk->code[chunk->count] = byte;
     chunk->count++;
 }
+
+int addConstant(Chunk* chunk, Value value) {
+    writeValueArray(&chunk->constants, value);
+    return chunk->constants.count - 1;
+}
+
+
