eb1a09723ba6bff10fa3a13ab65e304c30003c38
Ragib Asif <182296466+ragibasif@users.noreply.github.com>
2025-02-23 00:19:53 -0500

    {{UPDATE}} Sun Feb 23 00:19:53 2025

--- crafting-interpreters-clox/value.c
+++ crafting-interpreters-clox/value.c
@@ -13,7 +13,7 @@ void writeValueArray(ValueArray* array, Value value) {
     if (array->capacity < array->count +1) {
         int oldCapacity = array->capacity;
         array->capacity = GROW_CAPACITY(oldCapacity);
-        array->values = GROW_ARRAY(value, array->values, oldCapacity, array->capacity);
+        array->values = GROW_ARRAY(Value, array->values, oldCapacity, array->capacity);
     }
     array->values[array->count] = value;
     array->count++;
